f04385e1762d5dddf07296d5880a7deb498f8648
Javohir <javohirsadullayev836@gmail.com>
2024-11-21 21:34:34 +0500

    full

--- exercise_12.py
+++ exercise_12.py
@@ -3,7 +3,7 @@ row = int(input('Uzunlik: '))
 
 for i in range(1, row + 1):
     for j in range(1, col + 1):
-        if (j + i) % 2 == 0:
+        if i == 1 or j == 1 or j == col or i == row or i == j or i + j == row + 1:
             print("0", end=" ")
         else:
             print("1", end=" ")
